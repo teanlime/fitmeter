a87d3a0b8c43bb8ad2b04f74c5da735bbd0e66cf
tea.n.lime <tea.n.lime@gmail.com>
2024-05-22 23:34:17 +0100

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,6 @@ dependencies {
   androidTestImplementation(libs.androidx.espresso.core)
   androidTestImplementation(platform(libs.androidx.compose.bom))
   androidTestImplementation(libs.androidx.ui.test.junit4)
-//  testImplementation(project("FitMeter.app.main"))
-//  androidTestImplementation(project("FitMeter.app.main"))
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
 }
