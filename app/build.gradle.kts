f00e14ac6655f52c52c3602e81dcca202da70c18
tea.n.lime <tea.n.lime@gmail.com>
2024-05-23 19:09:34 +0100

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.jetbrains.kotlin.android)
+  // Add the Google services Gradle plugin
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -47,20 +49,23 @@ android {
 }
 
 dependencies {
-
+  implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
-  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.material3)
   implementation(libs.androidx.ui)
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.ui.tooling.preview)
-  implementation(libs.androidx.material3)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(platform(libs.firebase.bom))
+
   testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
+
   androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.ui.test.junit4)
-  debugImplementation(libs.androidx.ui.tooling)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+
   debugImplementation(libs.androidx.ui.test.manifest)
+  debugImplementation(libs.androidx.ui.tooling)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+
+    // Add the dependency for the Google services Gradle plugin
+    alias(libs.plugins.google.services) apply false
 }
