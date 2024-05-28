2482b3560c59e6145bee0858499d01381be0d3c0
tea.n.lime <tea.n.lime@gmail.com>
2024-05-28 20:22:46 +0100

    Set initial Weight widget

--- app/src/main/java/com/teanlime/wellscore/MainActivity.kt
+++ app/src/main/java/com/teanlime/wellscore/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.teanlime.wellscore.track.ui.TrackComposable
 import com.teanlime.wellscore.ui.theme.WellScoreTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,21 +27,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainActivityComposable() {
   WellScoreTheme {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-      Greeting(
-        name = "Android",
-        modifier = Modifier.padding(innerPadding)
-      )
-    }
+    Scaffold(
+      modifier = Modifier.fillMaxSize(),
+      content = { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+          TrackComposable()
+        }
+    })
   }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
 }
 
 @Preview(showBackground = true)
