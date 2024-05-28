1f3524c4b04b00f02c236566c39272da8e8d3b25
tea.n.lime <tea.n.lime@gmail.com>
2024-05-28 21:08:08 +0100

    Add empty model class

---
+++ app/src/main/java/com/teanlime/wellscore/model/track/TrackModel.kt
@@ -0,0 +1,66 @@
+package com.teanlime.wellscore.model.track
+
+typealias MetricType = String
+
+data class Tracked (
+  val metric: Metric, // Metric.WEIGHT
+  val upperBound: Double, // “400.0”
+  val lowerBound: Double, // “0.0”
+
+  val value: Double) { // “65.6”
+
+  fun increment(value: Double, incrementBy: Double = metric.increments): Tracked {
+    return this
+  }
+  fun decrement(value: Double, decrementBy: Double = metric.increments): Tracked {
+    return this
+  }
+  fun setTo(value: Double): Tracked {
+    return this
+  }
+}
+
+data class Metric (
+  val id: MetricType, // “Weight”
+  val displayNameSuffix: String, //”kg”
+  val displayNamePrefix: String, // “”
+  val increments: Double, // “0.1”
+  val normalizer: MetricNormalizer,  //MetricNormalizer.None<Double>
+  val decimals: Int, // “1”
+)
+
+sealed class MetricNormalizer {
+  abstract fun convertToDisplayValue(value: Double): Double
+  abstract fun convertFromDisplayValue(value: Double): Double
+
+  data object None: MetricNormalizer() {
+    override fun convertToDisplayValue(value: Double) = value
+    override fun convertFromDisplayValue(value: Double) = value
+  }
+}
+
+sealed class MetricSystemDefinitions(
+  val id: MetricType, // “Metric”
+  val displayName: String, // “Metric”
+  val definitions: Map<MetricType, Metric>
+)
+{
+  data object MetricSystemDefinition: MetricSystemDefinitions(
+    id = METRIC,
+    displayName = "Metric",
+    definitions = mapOf(
+      WEIGHT to Metric(
+        id = WEIGHT,
+        displayNameSuffix = "kg",
+        displayNamePrefix = "",
+        increments = 0.1,
+        decimals = 1,
+        normalizer = MetricNormalizer.None
+    )
+  ))
+
+  companion object {
+    val METRIC: MetricType = "Metric"
+    val WEIGHT: MetricType = "Weight"
+  }
+}
